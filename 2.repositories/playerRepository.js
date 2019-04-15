a30a1bd9cd8a95fb5f2027bdfd63b88dafdd4404
Asmir <asmir.ibrovic@gmail.com>
2019-04-15 17:47:02 +0200

    Update playerRepository.js

--- 2.repositories/playerRepository.js
+++ 2.repositories/playerRepository.js
@@ -2,12 +2,12 @@ function PlayerRepository() {
 
     this.getPlayer = async function (id) {
         var result = null;
-        var key = "97c4jrzgb9p2m3mqgj9sjcp5";
+        // var key = "97c4jrzgb9p2m3mqgj9sjcp5";
         // var key = "h5a88tvkth6ed5ms35davy6e";
        
 
         try {
-            var url = await fetch(`https://api.sportradar.us/soccer-t3/eu/en/players/${id}/profile.json?api_key=${key}`);
+            var url = await fetch(`http://127.0.0.1:3000/player/${id}`);
             var response = await url;
             result = await response.json();
 
