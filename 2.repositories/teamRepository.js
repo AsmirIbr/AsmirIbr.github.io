228a61a3ecada58cf88c14268d4e173c7e1360d2
Asmir <asmir.ibrovic@gmail.com>
2019-04-15 19:49:54 +0200

    saddasd

--- 2.repositories/teamRepository.js
+++ 2.repositories/teamRepository.js
@@ -2,24 +2,35 @@ function TeamRepository() {
 
     this.getTeamStatistics = async function (season) {
         var result = null;
+
+        $.ajax({
+            method: 'GET',
+            crossDomain:true,
+            url: `http://18.217.224.98:3000/teamstats/${season}`,
+            data: JSON.stringify(),
+        }).then(async(success) => {
+            result = await success.json();
+        },function(error){
+            // responseText.innerHTML = 'Не постои уред со таков сериски број, обидете се повторно';
+        });
        
-        try {
-            var url = await fetch(`http://18.217.224.98:3000/teamstats/${season}`); // za statistika
-            var response = await url;
-            result = await response.json();
-            // console.log("team repo:", result)
+        // try {
+        //     var url = await fetch(`http://18.217.224.98:3000/teamstats/${season}`); // za statistika
+        //     var response = await url;
+        //     result = await response.json();
+        //     // console.log("team repo:", result)
 
-            return new Team(result, season);
+        //     return new Team(result, season);
 
-        } catch (error) {
-            return result;
-        }
+        // } catch (error) {
+        //     return result;
+        // }
     }
 
     this.getTable = async function (season) {
         var result = null;
 
-        
+
         $.ajax({
             method: 'GET',
             crossDomain:true,
