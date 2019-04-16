203110338fcbcd6172d0edf792813f6019f0a55c
Asmir <asmir.ibrovic@gmail.com>
2019-04-16 21:31:17 +0200

    ...

--- 2.repositories/playerRepository.js
+++ 2.repositories/playerRepository.js
@@ -2,9 +2,8 @@ function PlayerRepository() {
 
     this.getPlayer = async function (id) {
         var result = null;
-       
         try {
-            var url = await fetch(`http://18.223.99.198:3000/player/${id}`);
+            var url = await fetch(`http://18.223.99.198:3000/players/${id}`);
             var response = await url;
             result = await response.json();
 
--- 2.repositories/teamRepository.js
+++ 2.repositories/teamRepository.js
@@ -7,7 +7,7 @@ function TeamRepository() {
             var url = await fetch(`http://18.223.99.198:3000/teamstats/${season}`); // za statistika
             var response = await url;
             result = await response.json();
-            // console.log("team repo:", result)
+            console.log(result)
 
             return new Team(result, season);
 
@@ -31,43 +31,30 @@ function TeamRepository() {
         //     console.log(error)
         // });
 
-        $.ajax({
-            url: `http://18.223.99.198:3000/table/${season}`,
-            contentType: 'application/json; charset: utf-8',
-            dataType: 'json',
-            data: JSON.stringify(),
-            type: 'GET',
-            success: ((res) =>{
-                console.log("Result: ", res)
-                // for (let i = 0; i < 7; i++) {
-                //    $(".chat").append($("<div>").html(res[i].msg));
-                  
-                // } 
-                // res.forEach(message => {
-                //   console.log(message.msg)
-                //   $(".chat").append($("<div>").html(message.msg));
-                // });
-                // $(".chat").html()
-            }),
-            error: ((error) =>{
-                console.log("Error: ", error);
-            })
-        });
+        //  $.ajax({
+        //             method: 'GET',
+        //             crossDomain:true,
+        //             url: `http://18.223.99.198:3000/table/${season}`,
+        //             data: JSON.stringify(),
+        //         }).then(function(success){
+        //             // responseText.innerHTML = success;
+        //             console.log(success)
+        //         },function(error){
+        //             console.log(error)
+        //         });
 
+        try {
+            var url = await fetch(`http://18.223.99.198:3000/tables/${season}`);
+            var response = await url;
+            result = await response.json();
 
-        // var url = `18.223.99.198:3000/table/${season}`
-        // try {
-        //     var url = await fetch("http://" + url);
-        //     var response = await url;
-        //     result = await response.json();
+            // console.log(result)
 
-        //     console.log("team repo:", result.season.name)
+            return new Table(result);
 
-        //     return new Table(result);
-
-        // } catch (error) {
-        //     return result;
-        // }
+        } catch (error) {
+            return result;
+        }
     }
 }
 
